f0663881f90d190a9371c104c86dd68620c5be71
Arya Anantula <anantula.arya@gmail.com>
2019-09-19 19:58:49 -0500

    Vuforia

---
+++ TeamCode/src/main/java/OmNomNom/VuforiaBitmapTest.java
@@ -0,0 +1,23 @@
+package OmNomNom;
+
+import Snack.VuforiaBitmap;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "VuforiaBitmapTest", group = "4546")
+public class VuforiaBitmapTest extends LinearOpMode {
+
+    private VuforiaBitmap sample;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        VuforiaBitmap sample = new VuforiaBitmap(this);
+
+        while (!isStarted()){
+            telemetry.addData("avgX: ", sample.avgX());
+            telemetry.update();
+            sleep(1000);
+        }
+        waitForStart();
+    }
+}
---
+++ TeamCode/src/main/java/Snack/VuforiaBitmap.java
@@ -0,0 +1,146 @@
+package Snack;
+
+import android.graphics.Bitmap;
+
+import static android.graphics.Color.red;
+import static android.graphics.Color.green;
+import static android.graphics.Color.blue;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.vuforia.Frame;
+import com.vuforia.Image;
+import com.vuforia.PIXEL_FORMAT;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.Parameters;
+
+import java.util.ArrayList;
+import java.util.concurrent.BlockingQueue;
+
+public class VuforiaBitmap {
+
+    private LinearOpMode opMode;
+
+    private VuforiaLocalizer vuforia;
+    private Parameters parameters;
+    private CameraDirection CAMERA_CHOICE = CameraDirection.BACK;
+    private static final String VUFORIA_KEY = "Acwi41P/////AAABmXAF5Uahj0aglVwEx0GLTotkFwuYvGa385NRnC3GmFdHiha7BKdStHJwB6nj4zrSBLOJ0jGEICqTReR3LiErc63MaNJf8NR/J8TUk6MOaF8xM5fa5uDU3J/7/tys+Hu1G5nlncWy3gGsHrU8lwG/rL+G0R/caVfNp0GfRtpcH7LMLDZOslSc+URv9+IF8+C0jA4JzTfM4lRkOEcIqIyTs20EZC+W3QYI7o7n700hOwq+WpoG7qMgqcrgk3+B1/hTLICE3fodM/34CQjbEONYKpGbj8IOG714CeY9qyI6WhainXidKda/QAslXEvYCDvBCZoGW/4I3TaZAJUWAeD1l5SeL/m4nuJxV9Jmai/0/9Qn";
+
+    public static String skystonePosition = "notFound";
+
+    private final int RED_THRESHOLD = 35;
+    private final int GREEN_THRESHOLD = 35;
+    private final int BLUE_THRESHOLD = 35;
+
+    public VuforiaBitmap(LinearOpMode opMode) {
+
+        this.opMode = opMode;
+
+        int cameraMonitorViewId = this.opMode.hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", this.opMode.hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        params.vuforiaLicenseKey = VUFORIA_KEY;
+        params.cameraDirection = CAMERA_CHOICE;
+        vuforia = ClassFactory.getInstance().createVuforia(params);
+
+        Vuforia.setFrameFormat(PIXEL_FORMAT.RGB565, true);
+        vuforia.setFrameQueueCapacity(4);
+        vuforia.enableConvertFrameToBitmap();
+
+    }
+
+    public Bitmap getBitmap() throws InterruptedException {
+
+        VuforiaLocalizer.CloseableFrame picture;
+        picture = vuforia.getFrameQueue().take();
+        Image rgb = picture.getImage(1);
+
+        long numImages = picture.getNumImages();
+
+        opMode.telemetry.addData("Num images", numImages);
+        opMode.telemetry.update();
+
+        for (int i = 0; i < numImages; i++) {
+
+            int format = picture.getImage(i).getFormat();
+            if (format == PIXEL_FORMAT.RGB565) {
+                rgb = picture.getImage(i);
+                break;
+            } else {
+                opMode.telemetry.addLine("Didn't find correct RGB format");
+                opMode.telemetry.update();
+
+
+            }
+        }
+
+        Bitmap imageBitmap = Bitmap.createBitmap(rgb.getWidth(), rgb.getHeight(), Bitmap.Config.RGB_565);
+        imageBitmap.copyPixelsFromBuffer(rgb.getPixels());
+
+        opMode.telemetry.addData("Image width", imageBitmap.getWidth());
+        opMode.telemetry.addData("Image height", imageBitmap.getHeight());
+        opMode.telemetry.update();
+        opMode.sleep(500);
+
+        picture.close();
+
+
+        return imageBitmap;
+    }
+
+    public double getImageHeight() throws InterruptedException {
+        Bitmap bitmap = getBitmap();
+        return bitmap.getHeight();
+    }
+
+    public double getImageWidth() throws InterruptedException {
+        Bitmap bitmap = getBitmap();
+        return bitmap.getWidth();
+    }
+
+
+    public double avgX() throws InterruptedException {
+        double avgX = 0;
+        double avgY = 0;
+        Bitmap bitmap = getBitmap();
+        int skystonePixelCount = 0;
+        ArrayList<Integer> xValues = new ArrayList<>();
+        ArrayList<Integer> yValues = new ArrayList<>();
+
+        for (int y = 0; y < bitmap.getHeight(); y++) {
+            for (int x = 0; x < bitmap.getWidth() / 2; x++) {
+                int pixel = bitmap.getPixel(x, y);
+                if (red(pixel) <= RED_THRESHOLD && blue(pixel) <= BLUE_THRESHOLD && green(pixel) <= GREEN_THRESHOLD) {
+                    xValues.add(x);
+                    yValues.add(y);
+                }
+            }
+        }
+
+        for (int xCoor : xValues) {
+            avgX += xCoor;
+        }
+        for (int yCoor : yValues) {
+            avgY += yCoor;
+        }
+        avgX /= xValues.size();
+        avgY /= yValues.size();
+
+        if (avgX < 220) {
+            skystonePosition = "1 & 4";
+            opMode.telemetry.addData("skystonePosition: ", skystonePosition);
+        } else if (avgX < 430) {
+            skystonePosition = "2 & 5";
+            opMode.telemetry.addData("skystonePosition: ", skystonePosition);
+        } else {
+            skystonePosition = "3 & 6";
+            opMode.telemetry.addData("skystonePosition: ", skystonePosition);
+        }
+        opMode.telemetry.update();
+        opMode.sleep(1000);
+        return avgX;
+    }
+}
