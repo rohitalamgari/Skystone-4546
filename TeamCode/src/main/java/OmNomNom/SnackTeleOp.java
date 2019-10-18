ed73ac79c0098b6280c59f7e13823a1b93b0734f
rohitalamgari <rohit.alamgari@gmail.com>
2019-10-18 17:35:05 -0500

    Update SnackTeleOp.java

--- TeamCode/src/main/java/OmNomNom/SnackTeleOp.java
+++ TeamCode/src/main/java/OmNomNom/SnackTeleOp.java
@@ -2,7 +2,7 @@ package OmNomNom;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-//@TeleOp (name = "SnackTeleop", group = "4546")
+@TeleOp (name = "SnackTeleop", group = "4546")
 public class SnackTeleOp extends SnackOpMode{
     @Override
     public void init() {
@@ -11,7 +11,7 @@ public class SnackTeleOp extends SnackOpMode{
 
     @Override
     public void loop() {
-        driveTrainPower(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+        driveTrainPower(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
         //snackLift.mtrLift.setPower(gamepad2.right_stick_y);
         //snackCollector.intake(gamepad2.left_stick_y);
     }
