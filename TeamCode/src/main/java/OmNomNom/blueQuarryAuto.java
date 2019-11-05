b4b8ddbc2d337c4b2be4a5cb27dc545beaec86f9
Arya Anantula <anantula.arya@gmail.com>
2019-11-05 16:06:43 -0600

    auto changes

--- TeamCode/src/main/java/OmNomNom/blueQuarryAuto.java
+++ TeamCode/src/main/java/OmNomNom/blueQuarryAuto.java
@@ -23,59 +23,65 @@ public class blueQuarryAuto extends LinearOpMode {
             telemetry.addData("medX: ", sample.medX(true));
             telemetry.addData("skystonePos", skystonePos);
             telemetry.update();
-            sleep(1000);
+            //sleep(1000);
         }
         waitForStart();
         if(!isStopRequested()){
             //goAlternatePark(true);
             if (skystonePos == "1 & 4"){
                 drive.armUp();
-                sleep(500);
+                sleep(100);
                 drive.strafeRight(.3,200);
                 drive.moveGyro(.25,38,0);
-                sleep(500);
+                sleep(100);
                 drive.moveGyro(.25, 6, 0);
                 drive.armDown();
-                sleep(300);
+                sleep(100);
                 drive.moveGyro(-.25,10,0);
-                sleep(500);
-                drive.turn(-90,.25);
-                drive.moveGyro(.25,75,-90);
-                drive.moveGyro(-.25,5,-90);
+                sleep(100);
+                drive.turn(-90,.35);
+                drive.moveGyro(.35,70,-90);
+                drive.moveGyro(-.35,5,-90);
                 drive.armUp();
-                sleep(300);
-                drive.moveGyro(-.25,10,-90);
+                sleep(100);
+                drive.moveGyro(-.35,15,-90);
                 //drive.turn(0,.3);
             }
             else if (skystonePos == "2 & 5"){
                 drive.armUp();
                 drive.moveGyro(.25,28,0);
-                sleep(500);
-                drive.turn(90,.25);
-                drive.moveGyro(.25,12,90);
-                sleep(300);
-                drive.turn(0,0.25);
+                sleep(100);
+                drive.turn(90,.35);
+                drive.moveGyro(.25,14,90);
+                sleep(100);
+                drive.turn(0,0.35);
                 drive.moveGyro(.25,11,0);
                 drive.armDown();
-                sleep(500);
+                sleep(100);
                 drive.moveGyro(-.25, 10, 0);
-                drive.turn(-90, .25);
-                drive.moveGyro(.25,85,-90);
+                drive.turn(-90, .35);
+                drive.moveGyro(.35,85,-90);
                 drive.armUp();
-                sleep(300);
-                drive.moveGyro(-.25, 15,-90);
+                sleep(100);
+                drive.moveGyro(-.35, 20,-90);
             }
             else{
                 drive.armUp();
-                drive.moveGyro(.25,28,drive.gyroYaw());
-                sleep(500);
-                drive.turn(90,.25);
-                drive.moveGyro(.25,22.5,drive.gyroYaw());
-                drive.turn(0,0.25);
-                drive.moveGyro(.25,8,drive.gyroYaw());
+                drive.moveGyro(.25,28,0);
+                sleep(100);
+                drive.turn(90,.35);
+                drive.moveGyro(.25,24,90);
+                sleep(100);
+                drive.turn(0,0.35);
+                drive.moveGyro(.25,11,0);
                 drive.armDown();
-                //drive.turn(0,.3);
-            }
+                sleep(100);
+                drive.moveGyro(-.25, 10, 0);
+                drive.turn(-90, .35);
+                drive.moveGyro(.35,95,-90);
+                drive.armUp();
+                sleep(100);
+                drive.moveGyro(-.35, 20,-90);            }
 
         }
     }
