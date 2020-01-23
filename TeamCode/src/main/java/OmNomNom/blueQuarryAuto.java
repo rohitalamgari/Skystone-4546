53c410bfa1f12075359adbd32504c9541afbbc74
Arya Anantula <43790515+Arya333@users.noreply.github.com>
2020-01-23 17:55:00 -0600

    skystone auto

--- TeamCode/src/main/java/OmNomNom/blueQuarryAuto.java
+++ TeamCode/src/main/java/OmNomNom/blueQuarryAuto.java
@@ -37,27 +37,69 @@ public class blueQuarryAuto extends LinearOpMode {
             if (skystonePos == "1 & 4"){
 
                 //skystone 1
-
-                drive.strafeLeftInches(.4, 12);
                 lift.release();
-                drive.moveGyro(-.2, 4, 0);
+                drive.strafeLeftInches(.35, 2);
+                sleep(200);
+                drive.moveGyro(.4, 42, 0);
                 sleep(250);
-                drive.moveGyro(.4, 35, 0);
-                sleep(100);
                 lift.grab();
-                drive.moveGyro(-.4, 10, 0);
-                sleep(250);
+                drive.moveGyro(-.4, 14, 0);
+                sleep(100);
                 drive.turn(-90, .35);
-                drive.moveGyro(.4, 25, -90);
+                drive.moveGyro(.4, 52, -90);
+                lift.release();
+                drive.moveGyro(-.6, 10 + 90, -90);
+                sleep(100);
+
+                //skystone 2
+                drive.moveGyro(.32,6, -90);
+                drive.turn(0,.35);
+                sleep(100);
+                drive.moveGyro(.4, 26, 0);
+                lift.grab();
+                drive.moveGyro(-.4, 14, 0);
+                drive.turn(-90,.35);
+                sleep(100);
+                drive.moveGyro(.4,  10+ 84, -90);
+                lift.release();
+                sleep(100);
+                drive.moveGyro(-.6,10,-90);
 
             }
             else if (skystonePos == "2 & 5"){
 
+                //skystone 1
+                lift.release();
+                drive.strafeRightInches(.35, 1);
+                sleep(200);
+                drive.moveGyro(.4, 42, 0);
+                sleep(250);
+                lift.grab();
+                drive.moveGyro(-.4, 14, 0);
+                sleep(100);
+                drive.turn(-90, .35);
+                drive.moveGyro(.4, 72, -90);
+                lift.release();
+                drive.moveGyro(-.6, 11 + 84, -90);
+
 
 
             }
-            else{
+            else {
 
+                //skystone 1
+                lift.release();
+                drive.strafeRightInches(.35, 6);
+                sleep(200);
+                drive.moveGyro(.4, 42, 0);
+                sleep(250);
+                lift.grab();
+                drive.moveGyro(-.4, 14, 0);
+                sleep(100);
+                drive.turn(-90, .35);
+                drive.moveGyro(.4, 82, -90);
+                lift.release();
+                drive.moveGyro(-.6, 11 + 84, -90);
 
             }
 
